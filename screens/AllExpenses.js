c8d96f1fc009f15bc6259bf8811df498bca26dc9
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2022-05-12 23:55:07 -0400

    Add files via upload

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,7 +1,15 @@
-import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
-
+import { ExpensesContext } from '../store/expense-context';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { useContext } from 'react';
 function AllExpenses() {
-  return <ExpensesOutput expensesPeriod= "Total"/>;
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+  <ExpensesOutput 
+  expenses = {expensesCtx.expenses} 
+  expensesPeriod="Total" 
+  fallbackText= "No registered expenses found"
+  />
+  );
 }
 
 export default AllExpenses;
